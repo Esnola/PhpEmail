4f8789b5fdc48bc59938219d6ec7db7dbe78e497
raishid <67575679+raishid@users.noreply.github.com>
2021-08-19 22:42:26 -0400

    add database

---
+++ mailerphp.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 20-08-2021 a las 02:41:54
+-- Versión del servidor: 5.7.31
+-- Versión de PHP: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mailerphp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `config_correo`
+--
+
+DROP TABLE IF EXISTS `config_correo`;
+CREATE TABLE IF NOT EXISTS `config_correo` (
+  `host` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `puerto` int(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `correos_enviados`
+--
+
+DROP TABLE IF EXISTS `correos_enviados`;
+CREATE TABLE IF NOT EXISTS `correos_enviados` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `correo` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `asunto` varchar(255) NOT NULL,
+  `mensaje` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `data_correos`
+--
+
+DROP TABLE IF EXISTS `data_correos`;
+CREATE TABLE IF NOT EXISTS `data_correos` (
+  `correos` varchar(255) DEFAULT NULL,
+  `mensaje` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
